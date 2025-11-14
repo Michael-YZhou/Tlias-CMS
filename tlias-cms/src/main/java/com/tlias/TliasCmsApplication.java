3fc435180cc4a6e03b001bba9362f10a8cc22f32
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-11-14 18:39:13 +1100

    initial commit

---
+++ tlias-cms/src/main/java/com/tlias/TliasCmsApplication.java
@@ -0,0 +1,13 @@
+package com.tlias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TliasCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TliasCmsApplication.class, args);
+    }
+
+}

20788091425856c19bb70ee349e4cd41c1f6d3a2
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-11-15 21:48:12 +1100

    Complete the GET /depts API to find all departments

--- tlias-cms/src/main/java/com/tlias/controller/DeptController.java
+++ tlias-cms/src/main/java/com/tlias/controller/DeptController.java
@@ -1,7 +1,24 @@
 package com.tlias.controller;
 
+import com.tlias.pojo.Dept;
+import com.tlias.pojo.Result;
+import com.tlias.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DeptController {
+
+    @Autowired
+    private DeptService deptService;
+
+    @GetMapping("/depts")
+    public Result list() {
+        System.out.println( "find all departments");
+        List<Dept> deptList = deptService.findAll();
+        return Result.success(deptList);
+    }
 }
--- tlias-cms/src/main/java/com/tlias/service/DeptService.java
+++ tlias-cms/src/main/java/com/tlias/service/DeptService.java
@@ -1,4 +1,9 @@
 package com.tlias.service;
 
+import com.tlias.pojo.Dept;
+
+import java.util.List;
+
 public interface DeptService {
+    List<Dept> findAll();
 }

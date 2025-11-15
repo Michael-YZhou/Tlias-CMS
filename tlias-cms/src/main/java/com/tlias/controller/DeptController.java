8e23059931629411a86cb11dce9e4d69b6593966
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-11-16 00:04:09 +1100

    Create API to delete department by id

--- tlias-cms/src/main/java/com/tlias/controller/DeptController.java
+++ tlias-cms/src/main/java/com/tlias/controller/DeptController.java
@@ -4,6 +4,7 @@ import com.tlias.pojo.Dept;
 import com.tlias.pojo.Result;
 import com.tlias.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,12 @@ public class DeptController {
         System.out.println( "find all departments");
         List<Dept> deptList = deptService.findAll();
         return Result.success(deptList);
+    }
+
+    @DeleteMapping("/depts")
+    public Result delete(Integer id) {
+        System.out.println( "delete department by id: " + id);
+        deptService.deleteById(id);
+        return Result.success();
     }
 }
--- tlias-cms/src/main/java/com/tlias/mapper/DeptMapper.java
+++ tlias-cms/src/main/java/com/tlias/mapper/DeptMapper.java
@@ -1,6 +1,7 @@
 package com.tlias.mapper;
 
 import com.tlias.pojo.Dept;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -11,4 +12,7 @@ public interface DeptMapper {
 
     @Select("select id, name, create_time, update_time from dept order by update_time desc")
     List<Dept> findAll();
+
+    @Delete("delete from dept where id = #{id}")
+    void deleteById(Integer id);
 }

bd8d2ba9f5fedc5a135ae6d87d17345a2c36e411
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-11-17 21:38:37 +1100

    Refactor Employee page pagination using pageHelper.

--- tlias-cms/src/main/java/com/tlias/mapper/EmpMapper.java
+++ tlias-cms/src/main/java/com/tlias/mapper/EmpMapper.java
@@ -12,12 +12,34 @@ public interface EmpMapper {
     /**
      * Counts the total number of employees by performing a left join between the emp table
      * and the dept table based on the department ID.
-     *
+     * Apply LIMIT with range for pagination.
      * @return the total count of employees as a Long value
      */
-    @Select("select count(*) from emp e left join dept d on e.dept_id = d.id")
-    Long countEmp();
+//    @Select("select count(*) from emp e left join dept d on e.dept_id = d.id")
+//    Long countEmp();
+//
+//    @Select("""
+//        SELECT e.id,
+//               e.name,
+//               e.create_time,
+//               e.update_time,
+//               e.job,
+//               e.image,
+//               e.dept_id,
+//               e.entry_date AS entryDate,
+//               d.name AS deptName
+//        FROM emp e
+//        LEFT JOIN dept d ON e.dept_id = d.id
+//        ORDER BY e.update_time DESC
+//        LIMIT #{start}, #{pageSize}
+//        """)
+//    List<Emp> findPageEmp(Integer start, Integer pageSize);
 
+    /**
+     * Select all rows for pagination.
+     * No need to apply LIMIT because we use pageHelper in this case.
+     * @return list of employees
+     */
     @Select("""
         SELECT e.id,
                e.name,
@@ -31,7 +53,8 @@ public interface EmpMapper {
         FROM emp e
         LEFT JOIN dept d ON e.dept_id = d.id
         ORDER BY e.update_time DESC
-        LIMIT #{start}, #{pageSize}
         """)
-    List<Emp> findPageEmp(Integer start, Integer pageSize);
+    List<Emp> list();
+
+
 }
--- tlias-cms/src/main/java/com/tlias/service/impl/EmpServiceImpl.java
+++ tlias-cms/src/main/java/com/tlias/service/impl/EmpServiceImpl.java
@@ -1,5 +1,7 @@
 package com.tlias.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.tlias.mapper.EmpMapper;
 import com.tlias.pojo.Emp;
 import com.tlias.pojo.PageResult;
@@ -15,16 +17,34 @@ public class EmpServiceImpl implements EmpService {
     @Autowired
     private EmpMapper empMapper;
 
+//    @Override
+//    public PageResult<Emp> pagination(@RequestParam Integer page, Integer pageSize) {
+//        Long total = empMapper.countEmp();
+//
+//        if (total > 0) {
+//            Integer start = (page - 1) * pageSize;
+//            List<Emp> rows = empMapper.findPageEmp(start, pageSize);
+//            return new PageResult<>(total, rows);
+//        } else {
+//            return new PageResult<>(0L, null);
+//        }
+//    }
+
+    /**
+     * employee pagination query using pagehelper
+     * @param page page number
+     * @param pageSize page size
+     * @return page result
+     */
     @Override
     public PageResult<Emp> pagination(Integer page, Integer pageSize) {
-        Long total = empMapper.countEmp();
+        //
+        PageHelper.startPage(page, pageSize);
 
-        if (total > 0) {
-            Integer start = (page - 1) * pageSize;
-            List<Emp> rows = empMapper.findPageEmp(start, pageSize);
-            return new PageResult<>(total, rows);
-        } else {
-            return new PageResult<>(0L, null);
-        }
+        List<Emp> list = empMapper.list();
+
+        Page<Emp> p = (Page<Emp>) list;
+
+        return new PageResult<Emp>(p.getTotal(), p.getResult());
     }
 }
